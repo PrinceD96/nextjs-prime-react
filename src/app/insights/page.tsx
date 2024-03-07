071551caaf695f5bb784a330388aed6f0d41918f
Daniel Morales <daniel.a.moralesalt@gmail.com>
2024-03-06 20:50:26 -0500

    feat: implement basic tab menu

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Insights() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+      <h1>Insights</h1>
     </main>
   );
 }
